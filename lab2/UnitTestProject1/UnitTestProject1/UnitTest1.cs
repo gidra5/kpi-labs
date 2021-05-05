a0f154463fc2c56eb4415275c8cccd3f54b4596f
gidra5 <hranitel.52@gmail.com>
2021-05-05 16:39:17 +0300

    implement lab2

--- lab2/UnitTestProject1/UnitTestProject1/UnitTest1.cs
+++ lab2/UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IIG.PasswordHashingUtils;
+
 
 namespace UnitTestProject1
 {
@@ -7,8 +9,82 @@ namespace UnitTestProject1
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Test_Hashing_Equal_Strings_Produces_Equal_Hashes()
         {
+            var str = "madlmadlkasd';ol";
+            var hash1 = PasswordHasher.GetHash(str);
+            var hash2 = PasswordHasher.GetHash(str);
+
+            Assert.AreEqual(hash1, hash2);
+        }
+
+        [TestMethod]
+        public void Test_Hashing_Different_Strings_Produces_Different_Hashes()
+        {
+            var str1 = "madlmadlkasd';ol";
+            var str2 = "madlmadlkasd';ol6";
+            var hash1 = PasswordHasher.GetHash(str1);
+            var hash2 = PasswordHasher.GetHash(str2);
+
+            Assert.AreNotEqual(hash1, hash2);
+        }
+
+        [TestMethod]
+        public void Test_Hashing_With_0_Or_1_Mod_Throws()
+        {
+            var str = "madlmadlkasd';ol";
+
+            Assert.ThrowsException<System.ArgumentException>(() => PasswordHasher.GetHash(str, "", 0));
+            Assert.ThrowsException<System.ArgumentException>(() => PasswordHasher.GetHash(str, "", 1));
+        }
+
+        [TestMethod]
+        public void Test_Hashing_With_Empty_String_Salt_Dont_Change_Hash()
+        {
+            var str = "madlmadlkasd';ol";
+            var salt = "";
+            var hash1 = PasswordHasher.GetHash(str);
+            var hash2 = PasswordHasher.GetHash(str, salt);
+
+            Assert.AreEqual(hash1, hash2);
+        }
+
+        [TestMethod]
+        public void Test_Hashing_Equal_Strings_With_Custom_Salt_And_Mod_Produces_Equal_Hashes()
+        {
+            var str = "madlmadlkasd';ol";
+            var salt = "kndaslkd'dsd";
+            uint mod = 5321;
+            var hash1 = PasswordHasher.GetHash(str, salt, mod);
+            var hash2 = PasswordHasher.GetHash(str, salt, mod);
+
+            Assert.AreEqual(hash1, hash2);
+        }
+
+        [TestMethod]
+        public void Test_Hashing_Equal_Strings_With_Custom_Salt_Produces_Equal_Hashes()
+        {
+            var str = "madlmadlkasd';ol";
+            var salt = "kndaslkd'dsd";
+            var hash1 = PasswordHasher.GetHash(str, salt);
+            var hash2 = PasswordHasher.GetHash(str, salt);
+
+            Assert.AreEqual(hash1, hash2);
+        }
+
+        [TestMethod]
+        public void Test_Hashing_Equal_Strings_With_Different_Salt_And_Mod_Produces_Different_Hashes()
+        {
+            var str = "madlmadlkasd';ol";
+            var salt = "kndaslkd'dsd";
+            uint mod = 5321;
+            var hash1 = PasswordHasher.GetHash(str);
+            var hash2 = PasswordHasher.GetHash(str, salt);
+            var hash3 = PasswordHasher.GetHash(str, salt, mod);
+
+            Assert.AreNotEqual(hash1, hash2);
+            Assert.AreNotEqual(hash2, hash3);
+            Assert.AreNotEqual(hash1, hash3);
         }
     }
 }
